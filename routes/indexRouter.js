0e5664692f7cac22f78beebe5cfa47199d6085c9
Yuki Motokane <216834584+motokaneyuki@users.noreply.github.com>
2026-08-16 02:19:41 +0900

    feat: add open detail link to praise cards

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { randomUUID } from "node:crypto";
 
 export const indexRouter = Router();
 
@@ -6,12 +7,14 @@ const praises = [
   {
     text: "You are the cutest fluffball!",
     user: "Yuki",
-    date: new Date()
+    date: new Date(),
+    id: crypto.randomUUID(),
   },
   {
     text: "Best cat evurrr!",
     user: "Yuki",
-    date: new Date()
+    date: new Date(),
+    id: crypto.randomUUID(),
   }
 ];
 
@@ -24,6 +27,18 @@ indexRouter.get('/new', (req, res) => {
 })
 
 indexRouter.post('/new', (req, res) => {
-    praises.push({ text: req.body.praise, user: req.body.name, date: new Date() });
+    praises.push({ text: req.body.praise, user: req.body.name, date: new Date(), id: crypto.randomUUID() });
     res.redirect('/');
+})
+
+indexRouter.get('/message/:id', (req,res) => {
+    const { id } = req.params;
+    const message = praises.find((praise) => praise.id == id);
+
+    if (!message) {
+        res.status(404).send('Praise not found');
+        return;
+    }
+
+    res.render('message', {message: message});
 })
