863b75d7afe8bd43f5662c10d02f7fa9f5c04def
Yuki Motokane <216834584+motokaneyuki@users.noreply.github.com>
2026-08-16 23:03:04 +0900

    refactor: use randomUUID and add 404 guard

--- controller/praiseController.js
+++ controller/praiseController.js
@@ -48,7 +48,7 @@ export const createPraise = [
         }
 
         const { praise, name } = matchedData(req);
-        praises.push({ text: praise, user: name, date: new Date(), id: crypto.randomUUID() });
+        praises.push({ text: praise, user: name, date: new Date(), id: randomUUID() });
         res.redirect('/');
     }
 ]
@@ -70,6 +70,10 @@ export const getPraiseById = (req, res) => {
 export const getUpdateForm = (req, res) => {
     const { id } = req.params;
     const message = praises.find((praise) => praise.id == id);
+
+    if (!message) {
+        return res.status(404).send('Praise not found');
+    }
 
     res.render('update', {message: message});
 }
