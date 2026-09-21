a984e276be21799ed216ec9732e134f3e2f046fa
Yuki Motokane <216834584+motokaneyuki@users.noreply.github.com>
2026-09-21 16:34:04 +0900

    feat: add users table to seed script

--- db/populatedb.js
+++ db/populatedb.js
@@ -8,16 +8,30 @@ CREATE TABLE IF NOT EXISTS praises (
     date DATE
 );
 
+TRUNCATE TABLE praises RESTART IDENTITY;
+
 INSERT INTO praises (username, text, date)
 VALUES 
     ('Yuki', 'You are the cutest fluffball!', '2026-08-30'),
     ('Yuki', 'Best cat evurrr!', '2026-08-29');
+
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    username VARCHAR ( 255 ),
+    password VARCHAR ( 255 )
+);
 `
 
 async function main() {
     console.log('seeding...');
+    const connectionString = process.argv[2];
+
+    if (!connectionString) {
+        throw new Error('Database URL is required');
+    }
+
     const client = new Client({
-        connectionString: process.env.DATABASE_URL,
+        connectionString,
     });
     try {
         await client.connect();
@@ -25,6 +39,7 @@ async function main() {
         console.log('added to database');
     } catch(error) {
         console.error('error adding to database', error);
+        process.exitCode = 1;
     } finally {
         await client.end();
         console.log('done');
